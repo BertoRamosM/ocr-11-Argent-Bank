bbb986d759de3eceedb17013bb724fbe3ffd43c5
ARamos <albertramos902@gmail.com>
2024-04-12 10:30:17 +0100

    deleted test function

--- ocrP11-ArgentBank/Argent-Bank/src/App.jsx
+++ ocrP11-ArgentBank/Argent-Bank/src/App.jsx
@@ -5,18 +5,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Pages/HomePage/Home"
 import SignIn from './components/Pages/SignInPage/SignIn';
 import UserPage from './components/Pages/UserPage/UserPage';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
-import { fetchLogin } from './features/authSlice';
+
 
 
 function App() {
 
-  const dispatch = useDispatch()
 
-  useEffect(() => {
-      dispatch(fetchLogin({email: "albertramos902@gmail.com", password: "albert123", rememberMe: false  }))
-  },[dispatch])
 
   return (
     <>
